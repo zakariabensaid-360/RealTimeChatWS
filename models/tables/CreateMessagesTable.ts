a042dc994e4a663acc203a073c8abc2aa72f3966
Zakaria Bensaid <zakariabensaid58@gmail.com>
2026-09-14 08:27:35 -0200

    updated

--- models/tables/CreateMessagesTable.ts
+++ models/tables/CreateMessagesTable.ts
@@ -1,17 +1,22 @@
 import { db } from "../../server";
 
 interface Messages {
+    uuid: string
     messages: {
         message: string
         created_at:  string
-    }
+    }[]
 }
 
-export default function CreateUserTable() {
+export default function CreateMessagesTable() {
+    db.exec(`PRAGMA foreign_keys = ON;`);
+
     db.exec(`
         CREATE TABLE IF NOT EXISTS messages (
             uuid TEXT NOT NULL UNIQUE,
-            messages TEXT NOT NULL DEFAULT '[]'
+            user_id INTEGER NOT NULL,
+            messages TEXT NOT NULL DEFAULT '[]',
+            FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
         );
     `);
 }
